92d2b0e336ebb42b8455e7df5d5962e294ac5933
Paul D. Mendes <pauldmendes5@gmail.com>
2020-04-06 17:45:41 +0400

    Final commit

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask
 app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
+
 
 @app.route('/')
 def hello():
     return 'hello world'
+
+@app.route('/')
+def hello_name(name):
+    return 'hello '
 
 if __name__ == '__main__':
     app.run()
